024233cbe9d1f35db809d8da412f1641a8aedfc7
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2025-02-10 19:29:54 +0700

    More changes✨

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:url" content="https://faizdev.my.id/" />
+        <meta property="og:title" content="FaizBastomi | Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/image/profile.jpg" />
+        <link rel="icon" type="image/jpeg" sizes="16x16" href="/image/favicon_16.jpg" />
+        <link rel="icon" type="image/jpeg" sizes="32x32" href="/image/favicon_32.jpg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
