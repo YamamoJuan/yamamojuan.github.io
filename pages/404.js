4cd5144e5ccbdb5a2609e0c58372ff7a85306d45
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2025-02-26 21:14:53 +0700

    chore: add icon in 404

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonRunning } from "@fortawesome/free-solid-svg-icons";
 
 export default function Custom404() {
     return (
@@ -12,8 +14,9 @@ export default function Custom404() {
                     <h1 className="text-4xl md:text-6xl pr-3 mr-4 border-r-2">404</h1>
                     <div className="flex flex-col">
                         <h1 className="text-lg md:text-2xl">This page doesn't exist</h1>
-                        <Link className="text-sm md:text-lg hover:cursor-pointer text-[#c6c6c6]" href="/">
-                            Go <span className="hover:underline">Home</span>
+                        <Link className="text-sm md:text-lg hover:text-[#eba0ac] hover:underline hover:cursor-pointer text-[#c6c6c6]" href="/">
+                        <FontAwesomeIcon className="me-1" icon={faPersonRunning} fontSize={24} />
+                            Home
                         </Link>
                     </div>
                 </div>
