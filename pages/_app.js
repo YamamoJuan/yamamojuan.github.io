83f85047910c0613c7e3a739f47c8ee814815568
yamamojuan <yamamojuan@gmail.com>
2025-05-26 07:52:30 +0000

    add Footer component and apply it globally in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import '@/styles/globals.css';
+import Footer from '@/components/Footer'; // <- ini udah benar
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
